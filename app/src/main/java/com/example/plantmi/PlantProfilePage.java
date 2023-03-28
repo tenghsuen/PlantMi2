f1f67bb74d0130be39ae43859b3872502f3ff848
sihui2709 <Hazel3466!>
2023-03-28 16:30:50 +0800

    gallery

--- app/src/main/java/com/example/plantmi/PlantProfilePage.java
+++ app/src/main/java/com/example/plantmi/PlantProfilePage.java
@@ -1,10 +1,16 @@
 package com.example.plantmi;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -15,7 +21,7 @@ import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class PlantProfilePage extends AppCompatActivity {
-    Button openCamera, waterBtn, logoutBtn;
+    Button openCamera, waterBtn, logoutBtn, galleryBtn;
     ImageView imageView;
     TextView addImgText;
     private final int camera_code=100;
@@ -25,6 +31,7 @@ public class PlantProfilePage extends AppCompatActivity {
         setContentView(R.layout.activity_plantprofile);
         openCamera = findViewById(R.id.OpenCamera);
         waterBtn = findViewById(R.id.waterButton);
+        galleryBtn=findViewById(R.id.GalleryButton);
         logoutBtn=findViewById(R.id.logout);
         addImgText = findViewById(R.id.simpleTextView);
         imageView=findViewById(R.id.capturedImage);
@@ -36,20 +43,46 @@ public class PlantProfilePage extends AppCompatActivity {
                 finish();
             }
         });
+        final ActivityResultLauncher<Intent> launcherGallery = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if( result.getResultCode() == Activity.RESULT_OK
+                                && result.getData() != null){
+                            Uri photoUri = result.getData().getData();
+                            imageView.setImageURI(photoUri);
+
+                        }
+                    }
+                }
+        );
+        ActivityResultLauncher<Intent> launcherCamera = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Bundle b = result.getData().getExtras();
+                        Bitmap thumbnail = (Bitmap) b.get("data");
+                        imageView.setImageBitmap(thumbnail);
+                    }
+                }
+        );
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                launcherGallery.launch(intent);
+            }
+        });
         openCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(camera,camera_code);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                launcherCamera.launch(intent);
+
             }
         });
-        //waterBtn.setOnClickListener(new View.OnClickListener() {
-            //@Override
-            //public void onClick(View view) {
-                //Intent intent = new Intent(PlantProfilePage.this, PlantStatus.class);
-                //startActivity(intent);
-        //    }
-        //});
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,24 +93,15 @@ public class PlantProfilePage extends AppCompatActivity {
             }
         });
     }
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode,@Nullable Intent data){
-            super.onActivityResult(requestCode,resultCode,data);
-            if (requestCode==camera_code){
-                Bitmap image= (Bitmap)data.getExtras().get("data");
-                imageView.setImageBitmap(image);
-                addImgText.setVisibility(View.INVISIBLE);
+//        @Override
+//        protected void onActivityResult(int requestCode, int resultCode,@Nullable Intent data){
+//            super.onActivityResult(requestCode,resultCode,data);
+//            if (requestCode==camera_code){
+//                Bitmap image= (Bitmap)data.getExtras().get("data");
+//                imageView.setImageBitmap(image);
+//                addImgText.setVisibility(View.INVISIBLE);
+//
+//            }
+       }
 
-            }
-    }
 
-
-    //@Override
-    //protected void onActivityResult(int requestCode)
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        Bitmap photo=(Bitmap)data.getExtras().get("data");
-//        imageView.setImageBitmap(photo);
-//    }
-}
