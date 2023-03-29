81f5feda3305ecff4fb69b7aaf4c8b8ca175b583
MH <liewmhh@gmail.com>
2023-03-30 03:02:31 +0800

    Updated sliding function and Plant Status

--- app/src/main/java/com/example/plantmi/PlantStatus.java
+++ app/src/main/java/com/example/plantmi/PlantStatus.java
@@ -1,14 +1,31 @@
 package com.example.plantmi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PlantStatus extends AppCompatActivity {
+
+    View slidingBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plantstatus);
+
+        slidingBar=findViewById(R.id.plantStatusSlidingBar);
+
+        slidingBar.setOnTouchListener(new OnSwipeTouchListener(PlantStatus.this) {
+            public void onSwipeBottom() {
+                Intent i = new Intent(PlantStatus.this, PlantProfilePage.class);
+                startActivity(i);
+                overridePendingTransition( R.anim.slide_from_top, R.anim.slide_in_top );
+            }
+        });
     }
+
+
 }
