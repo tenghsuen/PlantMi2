94c1f570a308fd3be02622dae675e0b139abde68
Ivorine <ivorineivorine@gmail.com>
2023-04-07 21:30:05 +0800

    alot of updates

--- app/src/main/java/com/example/plantmi/EditPlant.java
+++ app/src/main/java/com/example/plantmi/EditPlant.java
@@ -5,27 +5,63 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class EditPlant extends AppCompatActivity {
-    EditText plantName, plantDescription;
+    TextInputEditText plantName, plantDescription;
     Button saveDetails;
+    DatabaseReference rootDatabaseReference;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editplant);
         plantName = findViewById(R.id.plantname);
         plantDescription = findViewById(R.id.plantdescription);
         saveDetails = findViewById(R.id.save);
-
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String userUID = user.getUid();
+        rootDatabaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(userUID);
         saveDetails.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(EditPlant.this, PlantStatus.class);
-                startActivity(intent);
+                String updatedName = plantName.getText().toString();
+                String updatedDesc = plantDescription.getText().toString();
+                if(TextUtils.isEmpty(updatedName)){
+                    if(TextUtils.isEmpty(updatedDesc)){
+                        Intent intent = new Intent(EditPlant.this,PlantStatus.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+                else{
+                    HashMap newUser = new HashMap<>();
+                    newUser.put("plantdesc",updatedDesc);
+                    newUser.put("plantname",updatedName);
+                    newUser.put("user",user.getEmail());
+                    rootDatabaseReference.updateChildren(newUser);
+
+                    Intent intent = new Intent(EditPlant.this, PlantStatus.class);
+                    startActivity(intent);
+                }
+
             }
         });
+
+
+
+
     }
 }
--- app/src/main/java/com/example/plantmi/RegisterPage.java
+++ app/src/main/java/com/example/plantmi/RegisterPage.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,11 +17,17 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterPage extends AppCompatActivity {
     Button registerBtn, loginBtn;
     FirebaseAuth mAuth;
     TextInputEditText addEmail, addPassword;
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+    private User user;
 
     @Override
     public void onStart() {
@@ -42,6 +49,8 @@ public class RegisterPage extends AppCompatActivity {
         loginBtn=findViewById(R.id.loginbutton);
         addEmail=findViewById(R.id.addEmail);
         addPassword=findViewById(R.id.addPassword);
+
+
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,6 +65,7 @@ public class RegisterPage extends AppCompatActivity {
                 String email,password;
                 email=addEmail.getText().toString();
                 password=addPassword.getText().toString();
+                user = new User(email,"Radish","Hi! I am your beloved pet!");
 
                 if(TextUtils.isEmpty(email)){
                     Toast.makeText(RegisterPage.this,"Enter email address", Toast.LENGTH_SHORT).show();
@@ -65,21 +75,37 @@ public class RegisterPage extends AppCompatActivity {
                     Toast.makeText(RegisterPage.this,"Enter password", Toast.LENGTH_SHORT).show();
                     return;
                 }
-
                 mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
                                     Toast.makeText(RegisterPage.this, "Account success!",Toast.LENGTH_SHORT).show();
+
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+
                                     Intent intent = new Intent(RegisterPage.this,LoginPage.class);
                                     startActivity(intent);
                                     finish();
                                 } else {
                                     // If sign in fails, display a message to the user.
-                                    Toast.makeText(RegisterPage.this, "User Registered Before",Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(RegisterPage.this, "Registration failed. Try a longer password!",Toast.LENGTH_SHORT).show();
                                 }
                             }
+
+                            private void updateUI(FirebaseUser currentUser) {
+                                if (user != null) {
+                                    firebaseDatabase = FirebaseDatabase.getInstance();
+                                    databaseReference = firebaseDatabase.getReference();//databasereference
+
+                                    String UID = currentUser.getUid();
+                                    DatabaseReference userRef = databaseReference.child("users");//Create child node reference
+                                    userRef.child(UID).setValue(user);//Insert value to child node
+                                }
+                            }
+
                         });
 
             }
